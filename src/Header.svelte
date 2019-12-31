fbc5c2704bae2bb9eed1407cd6f0afa59d9a15a5
Bohdan <bohdanimiela@gmail.com>
2019-12-31 12:13:32 +0100

    Change stylings

--- src/Header.svelte
+++ src/Header.svelte
@@ -8,22 +8,25 @@
 
 <style>
   header {
-    background: #D3CCE3;
-    background: -webkit-linear-gradient(to bottom, #E9E4F0, #D3CCE3);
-    background: linear-gradient(to bottom, #E9E4F0, #D3CCE3);
-    margin-bottom: 1rem;
-    padding: 1rem;
+    background: #230444;
+    margin: 1em 0;
+    padding: 1em;
+    font-size: 14px;
+    border-radius: 1em;
+    box-shadow:
+      5px 5px 9px hsla(269, 89%, 4%, .1),
+      -5px -5px 9px hsla(269, 89%, 60%, .1);
   }
 
   .main-heading {
     display: flex;
-    justify-content: flex-start;
+    justify-content: center;
     align-items: center;
   }
 
   h1 {
-    color: #4c00ff;
-    font-size: 2rem;
+    color: #ed8240;
+    font-size: 3.2em;
     font-weight: 300;
     line-height: 1.1;
     text-transform: uppercase;
@@ -31,30 +34,21 @@
   }
 
   h2 {
+    color: #d2ebe9;
     margin: 0;
-    font-size: .5rem;
-    text-align: left;
-    padding-left: .5rem;
+    font-size: .8em;
+    font-weight: 200;
+    text-align: center;
   }
 
   img {
-		height: 2rem;
-    padding: .5rem;
+		height: 2.5em;
+    padding: .5em;
   }
 
-  @media (min-width: 400px) {
-    h1 { font-size: 3rem;}
-    h2 {
-      font-size: 1rem;
-      padding-left: 1rem
-    }
-    img {
-      height: 3rem;
-      padding: 1rem;
-    }
-  }
   @media (min-width: 500px) {
-    h1{ font-size: 4rem;}
-    img { height: 4rem;}
+    h1 { font-size: 5em; }
+    img { height: 3rem; }
+    h2 { font-size: 1.3em; }
   }
 </style>
--- src/Main.svelte
+++ src/Main.svelte
@@ -23,7 +23,7 @@ let bpm = 120;
 			value={bpm}
 			on:input="{ event => setBothFromBPM(event.target.value)}"
 		/>
-		bpm
+		BPM
 	</label>
 
 	<label>
@@ -33,29 +33,34 @@ let bpm = 120;
 			value={ms}
 			on:input="{ event => setBothFromMs(event.target.value)}"
 		/>
-		ms
+		MS
 	</label>
 		At {bpm}bpm one quarter note have a duration of {ms}ms.
 </main>
 
 <style>
-main {
-		text-align: center;
-		padding: 1rem;
-		background: #D3CCE3;
-  	background: -webkit-linear-gradient(to bottom, #E9E4F0, #D3CCE3);
-		background: linear-gradient(to bottom, #E9E4F0, #D3CCE3);
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 1em 0;
+    background: #230444;
+    color: #d2ebe9;
+    border-radius: 1em;
+    box-shadow:
+      5px 5px 9px hsla(269, 89%, 4%, .1),
+      -5px -5px 9px hsla(269, 89%, 60%, .1);
+  }
 
-	input {
-		max-width: 5rem;
-		border-radius: 1rem;
-		border-color: #4c00ff;
-		outline-color: orange;
-		-moz-outline-radius: 1rem;
-	}
+  input {
+    color: #230444;
+    background: #d2ebe9;
+    width: 80px;
+    border-radius: 1em;
+    outline-color: #ed8240;
+    -moz-outline-radius: 1em;
+  }
 
-	label {
-		color: #4c00ff;
-	}
+  label {
+    color: #d2ebe9;
+  }
 </style>
