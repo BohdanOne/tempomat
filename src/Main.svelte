d35fc1e8e56cd526b903b8eaca8f95629482b9d8
Bohdan <bohdanimiela@gmail.com>
2019-12-31 11:29:14 +0100

    Move <main> to separate component

---
+++ src/Main.svelte
@@ -0,0 +1,61 @@
+<script>
+
+let bpm = 120;
+	let ms = 500;
+
+	function setBothFromBPM(value) {
+		bpm = +value;
+		ms = Math.round(60000 / bpm);
+	}
+
+	function setBothFromMs(value) {
+		ms = +value;
+		bpm = Math.round((60 / ms) * 1000);
+  }
+
+</script>
+
+<main>
+	<label>
+		<input
+			type="number"
+			min="1"
+			value={bpm}
+			on:input="{ event => setBothFromBPM(event.target.value)}"
+		/>
+		bpm
+	</label>
+
+	<label>
+		<input
+			type="number"
+			min="1"
+			value={ms}
+			on:input="{ event => setBothFromMs(event.target.value)}"
+		/>
+		ms
+	</label>
+		At {bpm}bpm one quarter note have a duration of {ms}ms.
+</main>
+
+<style>
+main {
+		text-align: center;
+		padding: 1rem;
+		background: #D3CCE3;
+  	background: -webkit-linear-gradient(to bottom, #E9E4F0, #D3CCE3);
+		background: linear-gradient(to bottom, #E9E4F0, #D3CCE3);
+	}
+
+	input {
+		max-width: 5rem;
+		border-radius: 1rem;
+		border-color: #4c00ff;
+		outline-color: orange;
+		-moz-outline-radius: 1rem;
+	}
+
+	label {
+		color: #4c00ff;
+	}
+</style>
