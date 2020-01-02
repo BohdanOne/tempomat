dd0992e39384475d8a5e95b7606dbfcaee1cf122
Bohdan <bohdanimiela@gmail.com>
2020-01-02 11:36:00 +0100

    Add TempoMarkings to display italian tempo markings name

--- src/Main.svelte
+++ src/Main.svelte
@@ -1,5 +1,6 @@
 <script>
 	import ValueBox from './ValueBox.svelte';
+	import TempoMarkings from './TempoMarkings.svelte';
 
 	let bpm = 120;
 	let ms = 500;
@@ -17,22 +18,26 @@
 </script>
 
 <main>
-	<ValueBox
-		valueName="Beats Per Minute"
-		value={ bpm }
-		on:input={ setBothFromBPM }
-	/>
-	<ValueBox
-		valueName="Milliseconds"
-		value={ ms }
-		on:input={ setBothFromMs }
-	/>
+	<div class="value-boxes">
+		<ValueBox
+			valueName="Beats Per Minute"
+			value={ bpm }
+			on:input={ setBothFromBPM }
+		/>
+		<ValueBox
+			valueName="Milliseconds"
+			value={ ms }
+			on:input={ setBothFromMs }
+		/>
+	</div>
+	<TempoMarkings bpm={ bpm } />
 
 		<!-- At {bpm}bpm one quarter note have a duration of {ms}ms. -->
 </main>
 
 <style>
-	main {
+	.value-boxes {
+		max-width: 600px;
 		display: flex;
 		justify-content: space-between;
 		overflow: visible;
---
+++ src/TempoMarkings.svelte
@@ -0,0 +1,45 @@
+<script>
+  export let bpm;
+  $: tempo = defineTempo(bpm);
+
+  function defineTempo(bpm) {
+      if (bpm < 25) return 'Larghissimo';
+      if (bpm < 45) return 'Grave';
+      if (bpm < 60) return 'Largo / Lento';
+      if (bpm < 66) return 'Larghetto';
+      if (bpm < 76) return 'Adagio';
+      if (bpm < 108) return 'Andante';
+      if (bpm < 120) return 'Moderato';
+      if (bpm < 156) return 'Allegro';
+      if (bpm < 176) return 'Vivace';
+      if (bpm < 200) return 'Presto';
+      if (bpm <= 200) return 'Prestissimo';
+      else return 'choose your tempo';
+    }
+</script>
+
+<section>
+  <p>
+    This is in range of <span>{ tempo }</span> tempo marking.
+  </p>
+</section>
+
+<style>
+  section {
+    font-weight: 300;
+    max-width: 600px;
+    text-align: center;
+    padding: 2em;
+    margin: 1em 0;
+    background: #230444;
+    color: #d2ebe9;
+    border-radius: 1em;
+    box-shadow:
+      5px 5px 9px hsla(269, 89%, 4%, .1),
+      -5px -5px 9px hsla(269, 89%, 60%, .1);
+  }
+
+  span {
+    font-style: italic;
+  }
+</style>
